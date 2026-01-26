0b5befe9fc25fc3d5a23fa889bfad05091d19a9b
David <dbj@magenta-aps.dk>
2026-01-26 13:12:18 +0100

    fix: update migration file index from 53 to 55 after merge with dev

--- backend/openstream/app/migrations/0055_slidetemplate_created_at_slidetemplate_updated_at.py
+++ backend/openstream/app/migrations/0055_slidetemplate_created_at_slidetemplate_updated_at.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("app", "0052_document_processing_status"),
+        ("app", "0054_slideshow_is_emergency_slideshow"),
     ]
 
     operations = [
