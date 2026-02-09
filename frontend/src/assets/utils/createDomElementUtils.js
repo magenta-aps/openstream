c2d44cee47bf0c9a820abb327aa0aca93f974bfa
Freja Fischer Nielsen <ffn@magenta-aps.dk>
2026-02-09 10:22:41 +0100

    Styling chip - wip

--- frontend/src/assets/utils/createDomElementUtils.js
+++ frontend/src/assets/utils/createDomElementUtils.js
@@ -1,12 +1,13 @@
 // SPDX-FileCopyrightText: 2025 Magenta ApS <https://magenta.dk>
 // SPDX-License-Identifier: AGPL-3.0-only
 
-export function addChip(containerId, chipClass, chipText, deleteCallBack) {
-
+export function addChip(containerId, chipText, deleteCallBack, chipClass="",) {
     const chipContainer = document.getElementById(containerId);
     if (!chipContainer) return;
 
     const chip = document.createElement('button');
     chip.className = `${chipClass} d-flex align-items-center gap-1`;
-
+    chip.innerText = chipText;
+    
+    chipContainer.appendChild(chip);
 }
